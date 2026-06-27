707ca6170147187c2722966249aa0921252a454a
Ivan Gvozdev <eeone228@gmail.com>
2026-06-27 19:23:12 +0300

    Export SEP calculation pipeline

--- src/radar/pipelines/__init__.py
+++ src/radar/pipelines/__init__.py
@@ -1,4 +1,4 @@
-﻿"""Calculation pipelines."""
+"""Calculation pipelines."""
 
 from radar.pipelines.erb import (
     ERB_MODEL_COMPONENT,
@@ -12,9 +12,17 @@ from radar.pipelines.gcr import (
     GcrPipelineResult,
     calculate_gcr_pipeline,
 )
+from radar.pipelines.sep import (
+    SEP_LET_COMPONENT,
+    SEP_MODEL_COMPONENT,
+    SEP_OUTPUT_TABLES_COMPONENT,
+    SEP_PIPELINE_COMPONENT,
+    SEP_SHIELDING_COMPONENT,
+    SepPipelineResult,
+    calculate_sep_pipeline,
+)
 from radar.pipelines.sep_protons import (
     GEOMAGNETIC_PENETRATION_COMPONENT,
-    SEP_MODEL_COMPONENT,
     SEP_PROTON_PIPELINE_COMPONENT,
     SepProtonPipelineResult,
     calculate_sep_proton_pipeline,
@@ -26,12 +34,18 @@ __all__ = [
     "GCR_MODEL_COMPONENT",
     "GCR_PIPELINE_COMPONENT",
     "GEOMAGNETIC_PENETRATION_COMPONENT",
+    "SEP_LET_COMPONENT",
     "SEP_MODEL_COMPONENT",
+    "SEP_OUTPUT_TABLES_COMPONENT",
+    "SEP_PIPELINE_COMPONENT",
     "SEP_PROTON_PIPELINE_COMPONENT",
+    "SEP_SHIELDING_COMPONENT",
     "ErbPipelineResult",
     "GcrPipelineResult",
+    "SepPipelineResult",
     "SepProtonPipelineResult",
     "calculate_erb_pipeline",
     "calculate_gcr_pipeline",
+    "calculate_sep_pipeline",
     "calculate_sep_proton_pipeline",
 ]
---
+++ tests/test_pipelines_package.py
@@ -0,0 +1,28 @@
+from radar.pipelines import (
+    SEP_LET_COMPONENT,
+    SEP_MODEL_COMPONENT,
+    SEP_OUTPUT_TABLES_COMPONENT,
+    SEP_PIPELINE_COMPONENT,
+    SEP_PROTON_PIPELINE_COMPONENT,
+    SEP_SHIELDING_COMPONENT,
+    SepPipelineResult,
+    SepProtonPipelineResult,
+    calculate_sep_pipeline,
+    calculate_sep_proton_pipeline,
+)
+
+
+def test_pipelines_package_exports_full_sep_pipeline() -> None:
+    assert SEP_PIPELINE_COMPONENT == "sep_pipeline"
+    assert SEP_MODEL_COMPONENT == "sep_model"
+    assert SEP_SHIELDING_COMPONENT == "sep_shielding"
+    assert SEP_LET_COMPONENT == "sep_let"
+    assert SEP_OUTPUT_TABLES_COMPONENT == "sep_output_tables"
+    assert SepPipelineResult.__name__ == "SepPipelineResult"
+    assert callable(calculate_sep_pipeline)
+
+
+def test_pipelines_package_keeps_legacy_sep_proton_pipeline_export() -> None:
+    assert SEP_PROTON_PIPELINE_COMPONENT == "sep_proton_pipeline"
+    assert SepProtonPipelineResult.__name__ == "SepProtonPipelineResult"
+    assert callable(calculate_sep_proton_pipeline)
